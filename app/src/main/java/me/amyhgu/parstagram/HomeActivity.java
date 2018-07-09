b18841a122077017f7fe3aa9ef4e0c29d068a1d3
Amy Gu <amygu@fb.com>
2018-07-09 16:13:03 -0700

    home page setup

---
+++ app/src/main/java/me/amyhgu/parstagram/HomeActivity.java
@@ -0,0 +1,35 @@
+package me.amyhgu.parstagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
+import me.amyhgu.parstagram.model.Post;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ParseQuery.getQuery(Post.class).findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); i++) {
+                        Log.d("HomeActivity", "Post[" + i + "] = "+ objects.get(i).getDescription());
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/me/amyhgu/parstagram/ParseApplication.java
@@ -0,0 +1,41 @@
+package me.amyhgu.parstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import me.amyhgu.parstagram.model.Post;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
+//        // Use for troubleshooting -- remove this line for production
+//        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+//
+//        // Use for monitoring Parse OkHttp traffic
+//        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+//        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
+//        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+//        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+//        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+//        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        final Parse.Configuration config = new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.app_id)) // should correspond to APP_ID env variable
+                .clientKey(getString(R.string.master_key))  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server(getString(R.string.server_url))
+                .build();
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(config);
+    }
+}
